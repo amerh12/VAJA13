9ac7cb253f2ffae4900a740b64bfd567c097ef95
amerh12 <91185105+amerh12@users.noreply.github.com>
2022-06-29 18:53:41 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naloga13Vaje2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int stevec = 1;
+            int vpisanoSt;
+            int vsota = 0;
+            do
+            {
+                Console.Write("Vpisi {0}. stevilo:", stevec);
+                vpisanoSt = Convert.ToInt32(Console.ReadLine());
+
+                vsota += vpisanoSt;
+
+                stevec++;
+
+            }while (stevec < 11);
+
+            Console.WriteLine(vsota);
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
